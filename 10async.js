00ce0e80f2a6640897d7e179ed4306a6ca77dcbf
lttgra002 <lttgra001@gmail.com>
2020-01-12 20:46:14 +0200

    async upload

--- 10async.js
+++ 10async.js
@@ -1,21 +1,68 @@
-//============HTTP REQUESTS=============
+// //============HTTP REQUESTS=============
 
-const getTodos = () =>{
+// //Create a variable
+//     const request = new XMLHttpRequest();
+
+// //Everytime there is a change in state this EventListener will fire
+
+// request.addEventListener("readystatechange", () => {
+//     //Will log to console everytime there is a change in state
+//     //Good to use this to check what is received in the console
+//     //MDN guide gives details on each state
+//     //console.log(request, request.readyState)
+    
+//     //Do an if check to see when the response is at stage 4, then change the response text
+//     //We should also check the response status, to see if there is an error. All the different statuses can be viewed on the MDN site
+//     if(request.readyState === 4 && request.status === 200){
+//         console.log(request.responseText)
+//     } else if(request.readyState === 4){
+//         //Adds in a little message, so the request reached stage but there was an error along the way
+//         console.log("There has been an error")
+//     }
+// });
+
+// //Sends the request to the source point
+//     request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+//     request.send();
+
+//WE CAN PLACE ALL THE ABOVE INTO A FUNCTION SO THAT IS REUSABLE I.E. EVERYTIME TODOS NEED TO BE CALLED
+//THIS IS ALL REPLICATED BELOW IN A FUNCTION
+
+//=================Request in Callback Function===============
+
+// //============HTTP REQUESTS=============
+
+//Create the funtion
+const getTodos = (callback) => {
 
     const request = new XMLHttpRequest();
 
     request.addEventListener("readystatechange", () => {
-        //console.log(request, request.readyState)
-        //Each stage has a number. 4 is when you receive the data
         if(request.readyState === 4 && request.status === 200){
-            console.log(request, request.responseText)
+            //add in the callback function with the arguments
+            callback(undefined, request.responseText)
         } else if(request.readyState === 4){
-            console.log("error")
+            callback("Couldn't fetch resource", undefined)
         }
-    })
+    });
 
-    request.open("GET", "https://jsonplaceholder.typicode.com/todoss/");
+    request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
     request.send();
-}
 
-getTodos()
+};
+
+console.log(1);
+console.log(2);
+
+//To run the code
+getTodos((err, data) => {
+    console.log("callback fired");
+    if(err){
+        console.log(err)
+    } else{
+        console.log(data)
+    }
+});
+
+console.log(4);
+console.log(5);
---
+++ Projects/Extras/function.js
@@ -0,0 +1,105 @@
+//EXPRESSION
+//When you create a variable for the function and store it inside
+
+// const speak = function(){
+//     console.log("hello");
+// };
+
+// speak();
+
+
+//DECLARATION
+
+// speak();
+
+// function speak(){
+//     console.log("hello")
+// };
+
+
+//Which to use?
+//It is suggested to use an expression. A declaration makes use of hoisting. 
+//Therefore with a declaration it is possible to call a function before defining it
+//Whereas, with an expression you need to define it before calling it
+
+//============ARGUMENTS & PARAMETERS
+//You can give a parameter a default value
+
+// const greet = function(name = "mario", time = "morning"){
+//     console.log(`Good ${time} ${name}`);
+// };
+
+// //defaults will be assigned when no arguments are entered
+// greet()
+
+//===========RETURN VALUE===========
+//Return statement is used, when you want to get a value 
+
+// let radius = 5;
+// let height = 8;
+
+// const calcArea = function(radius){
+//     return 3.14 * radius**2;
+// };
+
+// const calcVol = function(height){
+//     return calcArea(radius)*height;
+// };
+
+// console.log(calcVol(height))
+
+//============ARROW FUNCTIONS=======
+//=>Regular Function
+
+// const calcArea = function(radius){
+//     return 3.14 * radius**2;
+// };
+
+//=>Arrow Function
+
+// const calcArea = (radius) => 3.14 * radius**2;
+
+// //When you place brackets around you create an object and therefore need to use the return statement?
+
+// console.log(calcArea(9));
+
+//===========METHODS===============
+//A method is a function
+//Use dot notation on existing variable
+
+//=>Function
+
+//Normal
+const greet = function(){
+    return "hello";
+};
+//Arrow
+const arrowGreet = () => "helloArrow";
+
+console.log(greet());
+console.log(arrowGreet());
+
+//=>Method
+
+const name = "Grant";
+
+console.log(name.toUpperCase());
+
+//==========CALLBACK FUNCTION=======
+//When you use a function in the argument of another function
+
+const myFunc = (callBackFunc) => {
+    let value = 50;
+    callBackFunc(value);
+}
+
+myFunc(value => {console.log(value)})
+
+const myFunk1 = function(callBackFunc1){
+    let value = 60
+    callBackFunc1(value)
+};
+
+myFunk1(function(value){
+    console.log(value)
+})
